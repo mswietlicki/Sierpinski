0b28d04c6e6ad5ebce8e3fab20264f8f6cd52fec
MarcinSzyba <marcin.szyba@gmail.com>
2014-06-10 22:14:05 +0200

    Sierpinski_v1.0
    Pierwsza próba

---
+++ Sierp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Sierp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.liczbaZagniezdzen = new System.Windows.Forms.TextBox();
+            this.btnGenerowania = new System.Windows.Forms.Button();
+            this.btnCzyszczenia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 540);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Liczba zagnieżdżeń:";
+            // 
+            // liczbaZagniezdzen
+            // 
+            this.liczbaZagniezdzen.Location = new System.Drawing.Point(121, 533);
+            this.liczbaZagniezdzen.Name = "liczbaZagniezdzen";
+            this.liczbaZagniezdzen.Size = new System.Drawing.Size(37, 20);
+            this.liczbaZagniezdzen.TabIndex = 1;
+            this.liczbaZagniezdzen.Text = "5";
+            // 
+            // btnGenerowania
+            // 
+            this.btnGenerowania.Location = new System.Drawing.Point(185, 531);
+            this.btnGenerowania.Name = "btnGenerowania";
+            this.btnGenerowania.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerowania.TabIndex = 2;
+            this.btnGenerowania.Text = "Generuj";
+            this.btnGenerowania.UseVisualStyleBackColor = true;
+            this.btnGenerowania.Click += new System.EventHandler(this.btnGenerowania_Click);
+            // 
+            // btnCzyszczenia
+            // 
+            this.btnCzyszczenia.Location = new System.Drawing.Point(697, 530);
+            this.btnCzyszczenia.Name = "btnCzyszczenia";
+            this.btnCzyszczenia.Size = new System.Drawing.Size(75, 23);
+            this.btnCzyszczenia.TabIndex = 3;
+            this.btnCzyszczenia.Text = "Wyczyść";
+            this.btnCzyszczenia.UseVisualStyleBackColor = true;
+            this.btnCzyszczenia.Click += new System.EventHandler(this.btnCzyszczenia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.btnCzyszczenia);
+            this.Controls.Add(this.btnGenerowania);
+            this.Controls.Add(this.liczbaZagniezdzen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox liczbaZagniezdzen;
+        private System.Windows.Forms.Button btnGenerowania;
+        private System.Windows.Forms.Button btnCzyszczenia;
+    }
+}
+
---
+++ Sierp/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sierp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCzyszczenia_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Graphics grafika;
+            grafika = this.CreateGraphics();
+            grafika.Clear(System.Drawing.Color.White);
+        }
+
+        public void RysujTrojkat(System.Drawing.Graphics gdzie, Pen czym, int x1, int y1, int x2, int y2, int x3, int y3, int ile)
+        {
+            int x12 = (x1 + x2) / 2;
+            int x23 = (x2 + x3) / 2;
+            int x31 = (x3 + x1) / 2;
+            int y12 = (y1 + y2) / 2;    //współrzędne wewnętrznego trójkąta
+            int y23 = (y2 + y3) / 2;
+            int y31 = (y3 + y1) / 2;
+
+            gdzie.DrawLine(czym, x1, y1, x2, y2);
+            gdzie.DrawLine(czym, x2, y2, x3, y3);
+            gdzie.DrawLine(czym, x3, y3, x1, y1);
+            if(ile>0)
+            {
+                ile--;
+                RysujTrojkat(gdzie, czym, x1, y1, x12, y12, x31, y31, ile);
+                RysujTrojkat(gdzie, czym, x12, y12, x2, y2, x23, y23, ile);
+                RysujTrojkat(gdzie, czym, x23, y23, x3, y3, x31, y31, ile);
+            }
+
+        }
+
+        private void btnGenerowania_Click(object sender, EventArgs e)
+        {
+            int x1 = 10, y1 = 10, x2 = 750, y2 = 10, x3 = 500, y3 = 500;
+            int ileZagniezdzen = 0;
+            if(!int.TryParse(liczbaZagniezdzen.Text, out ileZagniezdzen))
+            {
+                MessageBox.Show(string.Format("Skąd niby mam wiedzieć ile wynosi {0}???", liczbaZagniezdzen.Text));
+            }
+            else
+            {
+                if(ileZagniezdzen > 10)
+                {
+                    if(MessageBox.Show("Jesteś pewien? Więcej niż 10 zagnieżdżeń i tak nie widać, a rysować będą się dość długo.", "Uwaga!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        System.Drawing.Graphics grafika;
+                        grafika = this.CreateGraphics();
+                        Pen pioro = new Pen(System.Drawing.Color.Black, 1);
+                        RysujTrojkat(grafika, pioro, x1, y1, x2, y2, x3, y3, ileZagniezdzen);
+                    }
+                }
+                else if(ileZagniezdzen < 1)
+                {
+                    MessageBox.Show("Ujemna lub zerowa ilość nie ma tu sensu.");
+                }
+                else
+                {
+                    System.Drawing.Graphics grafika;
+                    grafika = this.CreateGraphics();
+                    Pen pioro = new Pen(System.Drawing.Color.Black, 1);
+                    RysujTrojkat(grafika, pioro, x1, y1, x2, y2, x3, y3, ileZagniezdzen);
+                }
+            }
+        }
+    }
+}
